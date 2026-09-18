4f7e37a336d3561a174182dbac23a226ef3b647f
Ranjeet  kumar <patelranjeet1212@gmail.com>
2026-09-18 16:31:35 +0530

    Error Handling

--- src/components/dialog_add_photos.py
+++ src/components/dialog_add_photos.py
@@ -28,7 +28,7 @@ def add_photos_dialog():
     if st.session_state.photo_tab == 'camera' :
         cam_photo = st.camera_input('Take Snapshot',key='dialog_cam')
         if cam_photo:
-            st.session_state.attendance_imagess.append(Image.open(cam_photo)) 
+            st.session_state.attendance_images.append(Image.open(cam_photo)) 
             st.toast('photo Captured.')
             st.rerun()
 
